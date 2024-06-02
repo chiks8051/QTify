737879f0bde58d31a3413d68f3e53aae94738ce8
Kumar Harshvardhan <sharshu1999hraj@gmail.com>
2024-06-03 01:12:35 +0530

    Adding New Album

--- qtify/src/component/Homepage.jsx
+++ qtify/src/component/Homepage.jsx
@@ -7,6 +7,7 @@ const Homapage = () => {
     <>
       <Hero />
       <Section title="Top Albums" endpoint="https://qtify-backend-labs.crio.do/albums/top" />
+      <Section title="New Albums" endpoint="https://qtify-backend-labs.crio.do/albums/new" />
     </>
   );
 };
