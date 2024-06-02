a6f4bd64844aade2031bbe3bfe9ef05c6d4086f9
Kumar Harshvardhan <sharshu1999hraj@gmail.com>
2024-06-03 03:49:02 +0530

    Adding the tooltip

--- qtify/src/component/Card.jsx
+++ qtify/src/component/Card.jsx
@@ -6,23 +6,23 @@ import { CardActionArea } from "@mui/material";
 import Chip from "@mui/material/Chip";
 import styles from "./Card.module.css";
 import Typography from "@mui/material/Typography";
-
+import Tooltip from "@mui/material/Tooltip";
 
 const CardHolder = ({ album }) => {
-    
+  console.log(album.songs.length);
   return (
     <>
       <Card className={styles.card}>
-
         <CardActionArea className={styles.action}>
-
-          <CardMedia
-            className={styles.cardImg}
-            component="img"
-            height="170"
-            image={album.image}
-            alt={album.title}
-          />
+          <Tooltip title={`${album.songs.length}`} arrow placement="top">
+            <CardMedia
+              className={styles.cardImg}
+              component="img"
+              height="170"
+              image={album.image}
+              alt={album.title}
+            />
+          </Tooltip>
           <CardContent className={styles.cardContent}>
             <Chip
               className={styles.chips}
